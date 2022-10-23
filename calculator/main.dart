5a57d25c3a957d052d7ed37d96c951114fe666a6
bgebes <berkay.gebes@bil.omu.edu.tr>
2022-10-23 14:07:04 +0300

    refactor: change check to try-catch block && feat: askToExit function

--- calculator/main.dart
+++ calculator/main.dart
@@ -1,30 +1,40 @@
 import 'dart:io';
 
 void main() {
-  Map<String, dynamic> input = getInput();
-  double result = calc(input['number1'], input['operator'], input['number2']);
-
-  print(
-      "${input['number1']} ${input['operator']} ${input['number2']} = $result");
+  runApp();
 }
 
-Map<String, dynamic> getInput() {
+void runApp() {
+  for (int i = 0;; i++) {
+    Map<String, dynamic> input = getOperation();
+    dynamic result =
+        calc(input['number1'], input['operator'], input['number2']);
+
+    print(
+        "${input['number1']} ${input['operator']} ${input['number2']} = $result");
+
+    if (askToExit(i)) break;
+  }
+}
+
+Map<String, dynamic> getOperation() {
   stdout.write("Type an operation: ");
   List<String>? input = stdin.readLineSync()?.split(' ');
-  if (input?.length != 3 && !input!.contains(' ')) {
+
+  try {
+    return {
+      'number1': double.parse(input![0]),
+      'operator': input[1],
+      'number2': double.parse(input[2]),
+    };
+  } catch (Exception) {
     print(
         "You typed in wrong format! Try again! Format: {number1} {operator} {number2}");
-    return getInput();
+    return getOperation();
   }
-
-  return {
-    'number1': double.parse(input![0]),
-    'operator': input[1],
-    'number2': double.parse(input[2]),
-  };
 }
 
-double calc(double number1, String operator, double number2) {
+dynamic calc(double number1, String operator, double number2) {
   switch (operator) {
     case '+':
       return number1 + number2;
@@ -35,6 +45,16 @@ double calc(double number1, String operator, double number2) {
     case '/':
       return number1 / number2;
     default:
-      return number1;
+      print("You typed unavailable operator! Available Operators: + - * /");
+      return '?';
   }
+}
+
+bool askToExit(int i) {
+  if (i == 0 || (i % 3).floor() != 0) return false;
+
+  stdout.write("Would you like to exit?[Y-n]: ");
+  String res = stdin.readLineSync()?.toUpperCase() ?? 'N';
+
+  return res == 'Y';
 }

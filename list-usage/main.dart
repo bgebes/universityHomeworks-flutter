05e3aa934009fa86dee6c768e441f96ba44ddb13
bgebes <berkay.gebes@bil.omu.edu.tr>
2022-10-23 14:41:01 +0300

    refactor: cast int variables to double

--- list-usage/main.dart
+++ list-usage/main.dart
@@ -1,43 +1,44 @@
 import 'dart:io';
 
 void main() {
-  List<int> numbers = getNumbersFromConsoleAsList(10);
-  List<int> sortedNumbers_Lower = sortArray(numbers);
-  List<int> sortedNumbers_Higher = sortedNumbers_Lower.reversed.toList();
+  List<double> numbers = getNumbersFromConsoleAsList(10);
+  List<double> sortedNumbers_Lower = sortArray(numbers);
+  List<double> sortedNumbers_Higher = sortedNumbers_Lower.reversed.toList();
 
   print("\nNumbers: $numbers");
   print("Numbers that sorted by lower to higher: $sortedNumbers_Lower");
   print("Numbers that sorted by higher to lower: $sortedNumbers_Higher");
 }
 
-int getNumberFromConsole() {
+double getNumberFromConsole() {
   stdout.write("Type a number: ");
   String? temp = stdin.readLineSync();
 
   if (temp!.contains(' ') ||
-      temp.contains('.') ||
       temp.contains(',') ||
+      temp == '-0' ||
       temp.length < 1) {
-    print("You typed in wrong format! Try again!");
+    print(
+        "You typed in wrong format! Try again! Format: {number} Example: 1 or 1.5 or 0");
     return getNumberFromConsole();
   }
 
-  return int.parse(temp);
+  return double.parse(temp);
 }
 
-List<int> getNumbersFromConsoleAsList(int length) {
-  List<int> temp = [];
+List<double> getNumbersFromConsoleAsList(int length) {
+  List<double> temp = [];
 
   for (int i = 0; i < length; i++) {
-    int input = getNumberFromConsole();
+    double input = getNumberFromConsole();
     temp.add(input);
   }
 
   return temp;
 }
 
-List<int> sortArray(List<int> array) {
-  List<int> temp = [...array];
+List<double> sortArray(List<double> array) {
+  List<double> temp = [...array];
   temp.sort();
 
   return temp;

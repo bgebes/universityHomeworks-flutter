e03b71fe9212b96a14e14c00b54ab667c3a7e261
bgebes <berkay.gebes@bil.omu.edu.tr>
2022-11-23 13:13:39 +0300

    feat: startup_namer

---
+++ startup_namer/lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:startup_namer/parts/part2.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Welcome to Flutter',
+      theme: ThemeData(
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Colors.white,
+          foregroundColor: Colors.black,
+        ),
+      ),
+      home: const Part2(),
+    );
+  }
+}
